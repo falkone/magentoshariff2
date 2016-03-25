158bdbe23a5947bd1658160a5f12bf76b4a2d306
Ulbricht <falk.ulbricht@falkone.de>
2016-03-25 21:23:40 +0100

    fix xml error

--- Bock/Shariff.php
+++ Bock/Shariff.php
@@ -1,9 +1,11 @@
 <?php
 namespace Falkone\MagentoShariff\Block;
 
-class Shariff  extends \Magento\Catalog\Block\Product\View\AbstractView {
-    
-    public function getProductName() {
+class Shariff extends \Magento\Catalog\Block\Product\View\AbstractView
+{
+
+    public function getProductName()
+    {
         return $this->getProduct()->getName();
     }
 }
